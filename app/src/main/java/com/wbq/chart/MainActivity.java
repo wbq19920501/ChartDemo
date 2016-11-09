d0d589298ec4efdf584025ecd797d044dfc1f4ad
wbq501 <749937855@qq.com>
2016-11-09 13:05:59 +0800

    Initial commit

---
+++ app/src/main/java/com/wbq/chart/MainActivity.java
@@ -0,0 +1,50 @@
+package com.wbq.chart;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LineChart linechart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        init();
+        initdata();
+    }
+
+    private void initdata() {
+//        LineData lineDatas = getLineData(36, 100);
+    }
+
+
+
+    private void init() {
+        linechart = (LineChart) findViewById(R.id.linechart);
+    }
+
+
+}
---
+++ app/src/main/java/mpchartexample/custom/MyAxisValueFormatter.java
@@ -0,0 +1,26 @@
+package mpchartexample.custom;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.text.DecimalFormat;
+
+public class MyAxisValueFormatter implements IAxisValueFormatter
+{
+
+    private DecimalFormat mFormat;
+
+    public MyAxisValueFormatter() {
+        mFormat = new DecimalFormat("###,###,###,##0.0");
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return mFormat.format(value) + " $";
+    }
+
+    @Override
+    public int getDecimalDigits() {
+        return 1;
+    }
+}
